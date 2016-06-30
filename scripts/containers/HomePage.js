b22d63bc8beb7aa1ddf191c7e685392c6a5b1e4d
Mathieu Leplatre <mathieu@mozilla.com>
2016-06-30 10:06:53 +0200

    Use dedicated heroku instance

--- scripts/containers/HomePage.js
+++ scripts/containers/HomePage.js
@@ -7,7 +7,7 @@ import * as PollActions from "../actions/poll";
 
 function mapStateToProps(state) {
   return {
-    server: "https://kinto-leplatrem.herokuapp.com/v1",
+    server: "https://leplatrem-happiness.herokuapp.com/v1",
     bucket: "happiness"
   };
 }
