273e4c08dca70f37817739728c4e0ad497979f1b
Mathieu Leplatre <mathieu@mozilla.com>
2016-07-01 12:17:02 +0200

    Improve apparence of footer

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -18,11 +18,14 @@ export default class App extends Component {
         </main>
         <footer className="page-footer">
           <div className="container">
-            <h5><Link to={`/`}>Create your poll</Link></h5>
+            <Link className="btn waves-effect waves-light red lighten-3" to={`/`}>Create your poll</Link>
           </div>
           <div className="footer-copyright">
             <div className="container">
             © 2016 Powered by <a className="grey-text text-lighten-3" href="https://kinto.readthedocs.io">Kinto</a>
+            <a className="grey-text text-lighten-4 right" href="https://github.com/leplatrem/happiness">
+              Source code
+            </a>
             </div>
           </div>
         </footer>
